bde235b944762841515431da75bed2e397000fbb
seif <seif.eld.ehab@gmail.com>
2019-04-12 01:10:06 +0200

    now the user can upload financial bank safely

--- app/src/main/java/com/example/ss/financeForBusinessUserPack/addFinancialInfoActivity.java
+++ app/src/main/java/com/example/ss/financeForBusinessUserPack/addFinancialInfoActivity.java
@@ -1,15 +1,85 @@
 package com.example.ss.financeForBusinessUserPack;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ss.R;
 
 public class addFinancialInfoActivity extends AppCompatActivity {
+    EditText cardNumberEt,expairyMonthEt,expairyYearEt,cvvEt;
+    String cardNumber,expairyDate,cvv;
+    Button confirm;
+    addFinancialInfoActivityPresenter presenter;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_financial_info);
+        intializeFields();
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardNumber=cardNumberEt.getText().toString();
+                expairyDate = expairyMonthEt.getText()+"/"+expairyYearEt.getText();
+                cvv=cvvEt.getText().toString();
+
+                if(cardNumber.isEmpty() ||cardNumber.length()!=14){
+
+                    cardNumberEt.setError("this card number is not correct");
+                   // Log.e("card num",cardNumber.charAt(0) +".."+cardNumber.length()+"");
+
+                }
+                else if (expairyDate.length()<5){
+                    Toast.makeText(addFinancialInfoActivity.this, "your expairy date isnt correct", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(cvv.length()!=3){
+                    cvvEt.setError("this cvv number isnt correct");
+                }
+
+
+                else {
+                    if(cardNumber.charAt(0)=='5'){
+
+                        presenter.uploadFinancialData(cardNumber,expairyDate,cvv,progressDialog);
+
+
+                    }
+
+                    else if (cardNumber.charAt(0)=='4'){
+                        presenter.uploadFinancialData(cardNumber,expairyDate,cvv,progressDialog);
+
+
+                    }
+else{
+                        cardNumberEt.setError("this card number is not correct");
+
+
+                    }
+
+                }
+
+            }
+        });
+
+
+    }
+
+    private void intializeFields() {
+        cardNumberEt=findViewById(R.id.card_number);
+        expairyMonthEt=findViewById(R.id.expiary_month);
+        expairyYearEt=findViewById(R.id.expairy_year);
+        cvvEt=findViewById(R.id.cvv);
+        confirm=findViewById(R.id.confirm);
+        presenter=new addFinancialInfoActivityPresenter(this);
+        progressDialog=new ProgressDialog(this);
     }
 }
--- app/src/main/java/com/example/ss/financeForBusinessUserPack/addFinancialInfoActivityPresenter.java
+++ app/src/main/java/com/example/ss/financeForBusinessUserPack/addFinancialInfoActivityPresenter.java
@@ -1,4 +1,59 @@
 package com.example.ss.financeForBusinessUserPack;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import com.example.ss.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class addFinancialInfoActivityPresenter {
+
+    Context context;
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+    FirebaseAuth mAuth;
+
+    addFinancialInfoActivityPresenter(Context context)
+    {
+        this.context=context;
+        database= FirebaseDatabase.getInstance();
+        mAuth=FirebaseAuth.getInstance();
+        userRef=database.getReference().child("Users").child(mAuth.getUid()).child("finance");
+    }
+
+    void uploadFinancialData(String cardNumber , String expairyDate , String cvv, final ProgressDialog progressDialog){
+        progressDialog.setMessage("updating your financial data ..");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        userRef.child("card_number").setValue(cardNumber);
+        userRef.child("expairy_date").setValue(expairyDate);
+        userRef.child("cvv").setValue(cvv).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                progressDialog.dismiss();
+                sendUserToMain();
+            }
+        });
+
+
+
+    }
+
+    private void sendUserToMain() {
+
+    Intent i = new Intent (context, MainActivity.class);
+    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(i);
+
+
+    }
+
+
 }
