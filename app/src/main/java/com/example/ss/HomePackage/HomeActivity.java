13cc760b30a2a895cd251ad2a369e42310628e50
ahmedali20 <ahmedali199623@gmail.com>
2019-04-18 01:23:46 +0200

    update newsFeedRcyclerview class

--- app/src/main/java/com/example/ss/HomePackage/HomeActivity.java
+++ app/src/main/java/com/example/ss/HomePackage/HomeActivity.java
@@ -2,14 +2,26 @@ package com.example.ss.HomePackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.ss.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageButton imgButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        imgButton=findViewById(R.id.like_image_button);
+        imgButton.setOnClickListener(imgButtonHandler);
     }
+    View.OnClickListener imgButtonHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            imgButton.setBackgroundResource(R.drawable.ic_favorite_black_24dp);
+        }
+    };
 }
