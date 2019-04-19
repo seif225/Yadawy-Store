53f94a07051922a40dca313dd7ef4d24e1745006
seif <seif.eld.ehab@gmail.com>
2019-04-19 22:31:32 +0200

    -updated some UI -added AddProdcutActvity

---
+++ app/src/main/java/com/example/ss/ProductActivityPack/ProductActivity.java
@@ -0,0 +1,19 @@
+package com.example.ss.ProductActivityPack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ss.R;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+
+
+
+    }
+}
